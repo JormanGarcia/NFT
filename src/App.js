9e3bb63f04e42269f4e2fc9c1184db7d2a835e51
Jorman Garcia <JormanGarcia@github.com>
2021-10-23 16:59:21 -0400

    little refactoring

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import "./App.scss";
 import Categories from "./components/Categories";
-import { Footer } from "./components/Footer";
-import FooterLink from "./components/FooterLink";
+import Layout from "./components/Layout";
 import { LowBanner } from "./components/LowBanner/index";
-import { Navbar } from "./components/Navbar";
 import Profile from "./components/Profile/index";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <main class="main">
-        <Profile />
+    <Layout>
+      <Profile />
 
-        <Categories />
-        <LowBanner />
-      </main>
-      <Footer />
-    </div>
+      <Categories />
+      <LowBanner />
+    </Layout>
   );
 }
 
